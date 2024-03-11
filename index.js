f628b024444345798998f592ddd61238e1e868f9
Mohan duhan <135856544+Mohan-duhan@users.noreply.github.com>
2024-03-11 23:25:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
+
+
+app.set("views engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); //watch it again
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//we use let here rather than using const because we want make it varibale so that the posts can easily be deleted
+let posts = [
+    {
+        id: uuidv4(),
+        username: "Chandigarh University",
+        content: "Located in Mohali ,Punjab!"
+    },
+    {
+        id: uuidv4(),
+        username: "Mohan Singh ",
+        content: "I love coding!"
+    },
+    {
+        id: uuidv4(),
+        username: "apnacollege",
+        content: "Best coding learning platform!"
+    },
+];
+
+//to form an nice RESTFUL API's always use /resource name.
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs")
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, content });
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post });
+
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+})
+
+app.listen(port, () => {
+    console.log("listening to port : 8080")
+});
